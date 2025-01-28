c451a03d3260ff3ea8637b2c58fbc463a85c4016
KananAmir <amirovknn@gmail.com>
2025-01-28 16:18:13 +0400

    deployment

--- 05.expressjs-mongodb-project/client/src/App.jsx
+++ 05.expressjs-mongodb-project/client/src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from "axios"
+import { BASE_URL } from './constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
 
+  const getProducts = async () => {
+    try {
+      const response = await axios(`${BASE_URL}products`)
+      setProducts(response.data.data)
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+
+  if (products.length === 0) {
+    return <p>no product to show!</p>
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {products.length > 0 && products.map((product) => {
+          return <li key={product._id}>{product.name}</li>
+        })}
+      </ul>
     </>
   )
 }
